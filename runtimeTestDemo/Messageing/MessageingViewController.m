5578af2bc6699e618d0e19c65f45e094b052563e
guogang123 <843968901@qq.com>
2018-11-01 17:51:36 +0800

    消息机制

--- runtimeTestDemo/Messageing/MessageingViewController.m
+++ runtimeTestDemo/Messageing/MessageingViewController.m
@@ -18,6 +18,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // 创建一个对象
+    // Cat *carlan = [[Cat alloc] init];
+    
+    // 使用 Runtime 创建一个对象
+    // 根据类名获取到类
+    Class catClass = objc_getClass("Cat");
+    
+    // 通过类创建实例对象
+    // 如果这里报错, 请将 Build Setting -> Enable Strict Checking of objc_msgsend Calls 改为 NO
+    Cat *harlan = objc_msgSend(catClass, @selector(alloc));
+    
+    // 初始化对象
+    // harlan = [harlan init];
+    
+    // 通过 Runtime 初始化对象
+    harlan = objc_msgSend(harlan, @selector(init));
+    
+    // 调用对象方法
+    // [harlan eat];
+
+    // 通过 Runtime 调用对象方法
+    // 通过的这个方法没有声明只有实现所以这里会有警告
+    // 但是发送消息的时候会从方法列表里寻找方法
+    // 所以这个能够成功执行
+    objc_msgSend(harlan, @selector(eat));
+    
+    // 输出 : 2018-11-01 17:40:01.581116+0800 runtimeTestDemo[48526:1974305] 嘤嘤嘤
+    
+    // 当然, objc_megSend 可以传递参数
+    Cat *alex = objc_msgSend(objc_msgSend(objc_getClass("Cat"), sel_registerName("alloc")), sel_registerName("init"));
+    objc_msgSend(alex, @selector(run:),10);
+ 
+    
     // Do any additional setup after loading the view.
 }
 
